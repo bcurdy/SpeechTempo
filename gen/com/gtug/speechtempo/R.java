697f94992bde5856e4bc60226216dd463b6bf161
Benoit Curdy <bcurdy@gmail.com>
2011-04-16 16:22:27 +0100

    Speech to MediaPlayer

--- gen/com/gtug/speechtempo/R.java
+++ gen/com/gtug/speechtempo/R.java
@@ -15,13 +15,13 @@ public final class R {
         public static final int recording_button=0x7f020001;
     }
     public static final class id {
-        public static final int imageView1=0x7f050000;
+        public static final int imageButton1=0x7f050000;
+        public static final int view1=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
     }
 }
