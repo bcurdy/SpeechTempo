ffd84adfe3b85d62c5334c9a115cc467324176bc
Benoit Curdy <bcurdy@gmail.com>
2011-04-17 11:53:40 +0100

    Working prototype

--- src/com/gtug/speechtempo/Recorder.java
+++ src/com/gtug/speechtempo/Recorder.java
@@ -2,6 +2,10 @@ package com.gtug.speechtempo;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioRecord;
@@ -9,6 +13,7 @@ import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.media.audiofx.Visualizer;
 import android.os.Bundle;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,11 +22,25 @@ import android.widget.Toast;
 
 public class Recorder extends Activity implements OnClickListener {
 	private static final String TAG = "Recorder";
+	private boolean isRunning;
+	//Audio Configuration
 	private static final int frequency = 8000;
 	private static final int channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
 	private static final int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
+	private int recordBufferSize = AudioRecord.getMinBufferSize(frequency,
+			channelConfiguration, audioEncoding);
+	private int playBufferSize = AudioTrack.getMinBufferSize(frequency,
+			channelConfiguration, audioEncoding);
+	private AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+			frequency, channelConfiguration, audioEncoding, recordBufferSize);
+	private AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+			frequency, channelConfiguration, audioEncoding, playBufferSize,
+			AudioTrack.MODE_STREAM);
+	//Layout
 	ImageButton recordButton;
 	Visualizer mVisualizer;
+	VisualizerView mVisualizerView;
+
 
 	/** Called when the activity is first created. */
 	@Override
@@ -30,51 +49,124 @@ public class Recorder extends Activity implements OnClickListener {
 		setContentView(R.layout.main);
 		recordButton = (ImageButton) findViewById(R.id.imageButton1);
 		recordButton.setOnClickListener(this);
+		isRunning = false;
 	}
 
 	@Override
 	public void onClick(View v) {
 		Log.d(TAG, "onClick: " + v);
-		record();
+		if (isRunning) {
+			stop();
+		} else {
+			record();
+		}
 	}
 
 	public void record() {
-		int recordBufferSize = AudioRecord.getMinBufferSize(frequency,
-				channelConfiguration, audioEncoding);
-		int playBufferSize= AudioTrack.getMinBufferSize(frequency,
-				channelConfiguration, audioEncoding);
-		AudioRecord audioRecord = new AudioRecord(
-				MediaRecorder.AudioSource.MIC, frequency, channelConfiguration,
-				audioEncoding, recordBufferSize);
-		AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-				frequency, channelConfiguration, audioEncoding, playBufferSize,
-				AudioTrack.MODE_STREAM);
-
-		byte[] buffer = new byte[recordBufferSize];
+		isRunning = true;
 		audioRecord.startRecording();
 		audioTrack.play();
 		Toast.makeText(this, "recording started", Toast.LENGTH_LONG).show();
-		while (true) {		
-			int bufferReadResult = audioRecord.read(buffer, 0, recordBufferSize);
-			audioTrack.write(buffer, 0, bufferReadResult);
-			mVisualizer = new Visualizer(audioTrack.getAudioSessionId());
-			mVisualizer.setEnabled(true);
-            byte[] data = new byte[mVisualizer.getCaptureSize()];
-            mVisualizer.getFft(data);
-			Log.d("AudioStream", ""+data);
-		}
+		Thread thread = new Thread(new Runnable() {
+			public void run() {
+				setupVisualizerFxAndUI();
+				mVisualizer.setEnabled(true);
+				Log.d("AudioStream", "Recording" );
+				while (isRunning) { 
+					byte[] buffer = new byte[recordBufferSize];
+					int bufferReadResult = audioRecord
+							.read(buffer, 0, recordBufferSize);
+					audioTrack.write(buffer, 0, bufferReadResult);
+				}
+			}
+		});
+		thread.start();
+	}
+
+	public void stop() {
+		isRunning = false;
+		audioRecord.stop();
+		audioTrack.stop();
+		Toast.makeText(this, "recording stopped", Toast.LENGTH_LONG).show();
+	}
+
+	private void setupVisualizerFxAndUI() {
+		// Create a VisualizerView (defined below), which will render the
+		// simplified audio wave form to a Canvas.
+		mVisualizerView = (VisualizerView)findViewById(R.id.view1);
+
+		// Create the Visualizer object and attach it to our media player.
+		mVisualizer = new Visualizer(audioTrack.getAudioSessionId());
+		mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+		mVisualizer.setDataCaptureListener(
+				new Visualizer.OnDataCaptureListener() {
+					public void onWaveFormDataCapture(Visualizer visualizer,
+							byte[] bytes, int samplingRate) {
+						mVisualizerView.updateVisualizer(bytes);
+					}
+
+					public void onFftDataCapture(Visualizer visualizer,
+							byte[] bytes, int samplingRate) {
+					}
+				}, Visualizer.getMaxCaptureRate() / 2, true, false);
+	}
+
+}
+
+/**
+ * A simple class that draws waveform data received from a
+ * {@link Visualizer.OnDataCaptureListener#onWaveFormDataCapture }
+ */
+class VisualizerView extends View {
+	
+	public VisualizerView(Context context, AttributeSet attrs) {   
+	    super(context, attrs);  
+	    init();
 	}
 	
-	class VisualizerView extends View {
+	private byte[] mBytes;
+	private float[] mPoints;
+	private Rect mRect = new Rect();
 
-		public VisualizerView(Context context) {
-			super(context);
-			init();
-		}
-		
-		private void init() {
-			//
-		}
+	private Paint mForePaint = new Paint();
+
+	private void init() {
+		mBytes = null;
+
+		mForePaint.setStrokeWidth(1f);
+		mForePaint.setAntiAlias(true);
+		mForePaint.setColor(Color.rgb(0, 128, 255));
 	}
 
+	public void updateVisualizer(byte[] bytes) {
+		mBytes = bytes;
+		invalidate();
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+
+		if (mBytes == null) {
+			return;
+		}
+
+		if (mPoints == null || mPoints.length < mBytes.length * 4) {
+			mPoints = new float[mBytes.length * 4];
+		}
+
+		mRect.set(0, 0, getWidth(), getHeight());
+
+		for (int i = 0; i < mBytes.length - 1; i++) {
+			mPoints[i * 4] = mRect.width() * i / (mBytes.length - 1);
+			mPoints[i * 4 + 1] = mRect.height() / 2
+					+ ((byte) (mBytes[i] + 128)) * (mRect.height() / 2) / 128;
+			mPoints[i * 4 + 2] = mRect.width() * (i + 1) / (mBytes.length - 1);
+			mPoints[i * 4 + 3] = mRect.height() / 2
+					+ ((byte) (mBytes[i + 1] + 128)) * (mRect.height() / 2)
+					/ 128;
+		}
+
+		canvas.drawLines(mPoints, mForePaint);
+	}
 }
